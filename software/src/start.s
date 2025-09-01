629aeba2be70d3e049eceba385dc56ee80c83e19
feaksel <furkanemiraksel1@gmail.com>
2025-09-01 18:56:13 +0300

    Some changes

--- software/src/start.s
+++ software/src/start.s
@@ -2,15 +2,12 @@
 .global _start
 
 _start:
-    la sp, _stack_top
-    la t0, _bss_start
-    la t1, _bss_end
-clear_bss:
-    beq t0, t1, bss_done
-    sw zero, 0(t0)
-    addi t0, t0, 4
-    j clear_bss
-bss_done:
-    call main
+    # Set stack pointer
+    li sp, 0x80010000
+    
+    # Simple main call - NO BSS CLEARING
+    jal main
+    
+    # If main returns, hang
 hang:
     j hang
